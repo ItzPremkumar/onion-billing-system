89446538df12784fc5e130a50e3bd70709ff2a22
Premkumar Harar <142881084+ItzPremkumar@users.noreply.github.com>
2026-03-06 23:22:57 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
-<?php
- $conn=pg_connect("host=localhost port=5434 dbname=prem user=postgres password=root");
+<php
+ $conn = pg_connect("
+host=dpg-d6lguki4d50c73bakap0-a
+dbname=oniondb
+user=onionuser
+password=dpg-d6lguki4d50c73bakap0-a
+port=5432
+");
 if(!$conn)
 {
  echo("Data base connection fail");
 }
+
 ?>
